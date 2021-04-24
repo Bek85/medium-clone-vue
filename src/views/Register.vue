08902a32221752ccc0120cd998d080165f260ff3
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-24 06:11:32 +0500

    create auth module with initial properties

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -30,7 +30,10 @@
                 placeholder="Password"
               />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
               Sign Up
             </button>
           </form>
@@ -43,9 +46,14 @@
 <script>
 export default {
   name: 'McvRegister',
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting;
+    }
+  },
   methods: {
     onSubmit() {
-      console.log('submitted form');
+      this.$store.commit('registerStart');
     }
   }
 };
