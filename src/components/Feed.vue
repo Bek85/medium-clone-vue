0e719a06afb9aa41d86701a6489e6d6653f06e65
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-06-03 00:09:04 +0500

    dispatch getFeed action in Feed component to retrieve articles data

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -5,8 +5,13 @@
 </template>
 
 <script>
+import { actionTypes } from '@/store/modules/feed';
 export default {
   name: 'McvFeed',
+  mounted() {
+    console.log('init feed');
+    this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl });
+  },
   props: {
     apiUrl: {
       type: String,
