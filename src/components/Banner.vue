1ae1521572c49da2844e9e645e58e8bff503493c
Bekhzod <stx.webdev@gmail.com>
2023-11-23 10:04:56 +0500

    create <Banner /> component

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <h1>Medium Clone Vue</h1>
+      <p>A place to share knowledge</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvBanner',
+};
+</script>
--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-page">
-    BANNER
+    <McvBanner />
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
@@ -17,12 +17,14 @@
 <script>
 import McvFeed from '@/components/Feed.vue';
 import McvPopularTags from '@/components/PopularTags.vue';
+import McvBanner from '@/components/Banner.vue';
 
 export default {
   name: 'McvGlobalFeed',
   components: {
     McvFeed,
     McvPopularTags,
+    McvBanner,
   },
   data() {
     return {
